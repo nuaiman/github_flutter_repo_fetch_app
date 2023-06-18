e5ceb1a0925c2798da1a20045d67d693ffcbcd3a
unknown <ashiqnuaiman@gmail.com>
2023-06-18 21:38:09 +0600

    fourth commit, added model file

--- lib/model/github_repo_model.dart
+++ lib/model/github_repo_model.dart
@@ -1,1 +1,22 @@
-
+class GithubRepo {
+  final int id;
+  final String name;
+  final String description;
+  final String repoUrl;
+  final String createdAt;
+  final String updatedAt;
+  final int starCount;
+  final String ownerName;
+  final String ownerAvatarUrl;
+  GithubRepo({
+    required this.id,
+    required this.name,
+    required this.description,
+    required this.repoUrl,
+    required this.createdAt,
+    required this.updatedAt,
+    required this.starCount,
+    required this.ownerName,
+    required this.ownerAvatarUrl,
+  });
+}
