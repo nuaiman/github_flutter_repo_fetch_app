688b091d085bb112287936632336c91be2c4c45a
unknown <ashiqnuaiman@gmail.com>
2023-06-18 21:36:52 +0600

    third commit, added repository files

--- lib/repo/get_github_repos_repo.dart
+++ lib/repo/get_github_repos_repo.dart
@@ -1,1 +1,21 @@
-// 
+import 'package:http/http.dart';
+
+abstract class IGetGithubRepo {
+  Future<Response> getRepos();
+}
+// -----------------------------------------------------------------------------
+
+class GetGithubRepo implements IGetGithubRepo {
+  final Client _client;
+  GetGithubRepo({required Client client}) : _client = client;
+
+  @override
+  Future<Response> getRepos() async {
+    const uri =
+        'https://api.github.com/search/repositories?q={query=flutter}&per_page=50';
+    final data = await _client.get(Uri.parse(uri));
+
+    return data;
+  }
+}
+// -----------------------------------------------------------------------------
