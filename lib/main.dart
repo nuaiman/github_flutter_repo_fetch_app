48445b29c65ed781c43e4b1005fd6f91bcd99004
unknown <ashiqnuaiman@gmail.com>
2023-06-18 21:35:41 +0600

    second commit, added folder structure and packages and fixed minSDKVersion

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'features/get_github_repos/view/home_view.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -15,7 +17,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const Scaffold(),
+      home: const HomeView(),
     );
   }
 }
